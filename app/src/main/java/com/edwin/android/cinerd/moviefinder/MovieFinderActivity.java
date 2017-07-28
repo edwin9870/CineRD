d7f57f746860b1b92e7ffd2ad89fd5011f0f6f8d
Edwin Ramirez Ventura <edwin9870@gmail.com>
2017-07-27 23:13:41 -0400

    Create MovieFinderActivity

---
+++ app/src/main/java/com/edwin/android/cinerd/moviefinder/MovieFinderActivity.java
@@ -0,0 +1,15 @@
+package com.edwin.android.cinerd.moviefinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.edwin.android.cinerd.R;
+
+public class MovieFinderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_finder);
+    }
+}
