5b9fbc8fdf73fb509a7ccf62faad15dc770ca0b7
Edwin Ramirez Ventura <edwin9870@gmail.com>
2017-07-30 18:55:26 -0400

    Add TheaterActivity

---
+++ app/src/main/java/com/edwin/android/cinerd/theater/TheaterActivity.java
@@ -0,0 +1,15 @@
+package com.edwin.android.cinerd.theater;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.edwin.android.cinerd.R;
+
+public class TheaterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theater);
+    }
+}
