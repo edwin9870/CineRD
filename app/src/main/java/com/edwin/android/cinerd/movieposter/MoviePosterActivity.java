e9e25d7e74fe791b5fb3f4d3f31f239ca81a135f
Edwin Ramirez Ventura <edwin9870@gmail.com>
2017-08-02 15:45:21 -0400

    Implemented menu

--- app/src/main/java/com/edwin/android/cinerd/movieposter/MoviePosterActivity.java
+++ app/src/main/java/com/edwin/android/cinerd/movieposter/MoviePosterActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import com.edwin.android.cinerd.R;
 import com.edwin.android.cinerd.configuration.di.ApplicationModule;
@@ -13,6 +14,8 @@ import com.edwin.android.cinerd.configuration.di.DaggerDatabaseComponent;
 import com.edwin.android.cinerd.configuration.di.DatabaseComponent;
 import com.edwin.android.cinerd.data.adapters.AccountGeneral;
 import com.edwin.android.cinerd.movieposter.dialog.MovieTheaterDialogFragment;
+import com.hlab.fabrevealmenu.listeners.OnFABMenuSelectedListener;
+import com.hlab.fabrevealmenu.view.FABRevealMenu;
 
 import javax.inject.Inject;
 
@@ -23,13 +26,16 @@ import butterknife.OnClick;
 import static com.edwin.android.cinerd.util.ActivityUtil.parents;
 
 public class MoviePosterActivity extends AppCompatActivity implements
-        MovieTheaterDialogFragment.MovieTheaterDialogListener{
+        MovieTheaterDialogFragment.MovieTheaterDialogListener {
 
     public static final String TAG = MoviePosterActivity.class.getSimpleName();
     @BindView(R.id.floating_button_movie_menu)
     FloatingActionButton floatingButtonMovieMenu;
 
-    @Inject MoviePosterPresenter moviePosterPresenter;
+    @Inject
+    MoviePosterPresenter moviePosterPresenter;
+    @BindView(R.id.menu_fab_filter)
+    FABRevealMenu mFabFilterMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +43,20 @@ public class MoviePosterActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_movie_poster);
         ButterKnife.bind(this);
 
+        try {
+            if (floatingButtonMovieMenu != null && mFabFilterMenu != null) {
+                mFabFilterMenu.bindAncherView(floatingButtonMovieMenu);
+                mFabFilterMenu.setOnFABMenuSelectedListener(new OnFABMenuSelectedListener() {
+
+                    @Override
+                    public void onMenuItemSelected(View view) {
+
+                    }
+                });
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         AccountGeneral.createSyncAccount(this);
 
@@ -44,12 +64,12 @@ public class MoviePosterActivity extends AppCompatActivity implements
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         MoviePosterFragment fragment = MoviePosterFragment.newInstance();
 
-        DatabaseComponent databaseComponent = DaggerDatabaseComponent.builder().applicationModule(new
+        DatabaseComponent databaseComponent = DaggerDatabaseComponent.builder().applicationModule
+                (new
                 ApplicationModule(getApplication())).build();
         DaggerMoviePosterComponent.builder().databaseComponent(databaseComponent)
                 .moviePosterPresenterModule(new MoviePosterPresenterModule(fragment)).build()
                 .inject(this);
-
 
 
         fragmentTransaction.add(R.id.fragment_movie_poster, fragment);
