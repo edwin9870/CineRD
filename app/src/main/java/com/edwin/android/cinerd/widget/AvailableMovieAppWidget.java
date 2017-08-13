be103e126cebf9c0ad670b02928aaeefd50963d0
Edwin Ramirez Ventura <edwin9870@gmail.com>
2017-08-13 15:23:43 -0400

    Implemented widget

--- app/src/main/java/com/edwin/android/cinerd/widget/AvailableMovieAppWidget.java
+++ app/src/main/java/com/edwin/android/cinerd/widget/AvailableMovieAppWidget.java
@@ -1,22 +1,33 @@
-package com.edwin.android.cinerd;
+package com.edwin.android.cinerd.widget;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.widget.RemoteViews;
+
+import com.edwin.android.cinerd.R;
+import com.google.gson.Gson;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class AvailableMovieAppWidget extends AppWidgetProvider {
 
+    public static final String TAG = AvailableMovieAppWidget.class.getSimpleName();
+
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
+        Log.d(TAG, "Executing widget");
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.available_movie_app_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        Intent intent = new Intent(context, ListGridService.class);
+        views.setRemoteAdapter(R.id.list_movie, intent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -39,5 +50,13 @@ public class AvailableMovieAppWidget extends AppWidgetProvider {
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
     }
+
+    public static void updateMoviesDataWidget(Context context) {
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(context, AvailableMovieAppWidget.class));
+        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.list_movie);
+        Log.d(TAG, "Widget notified");
+    }
+
 }
 
---
+++ app/src/main/java/com/edwin/android/cinerd/widget/MovieRemoteViewFactory.java
@@ -0,0 +1,133 @@
+package com.edwin.android.cinerd.widget;
+
+import android.content.Context;
+import android.os.Binder;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.edwin.android.cinerd.R;
+import com.edwin.android.cinerd.configuration.di.ApplicationModule;
+import com.edwin.android.cinerd.configuration.di.DaggerDatabaseComponent;
+import com.edwin.android.cinerd.data.repositories.GenreRepository;
+import com.edwin.android.cinerd.data.repositories.MovieRepository;
+import com.edwin.android.cinerd.entity.Genre;
+import com.edwin.android.cinerd.entity.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Edwin Ramirez Ventura on 5/27/2017.
+ */
+
+public class MovieRemoteViewFactory implements RemoteViewsService.RemoteViewsFactory {
+
+    public static final String TAG = MovieRemoteViewFactory.class.getSimpleName();
+    private Context mContext;
+    private List<Movie> movies;
+    MovieRepository mMovieRepository;
+    GenreRepository mGenreRepository;
+
+    public MovieRemoteViewFactory(Context applicationContext) {
+        mContext = applicationContext;
+        mMovieRepository = DaggerDatabaseComponent.builder()
+                .applicationModule
+                        (new ApplicationModule(applicationContext)).build().getMovieRepository();
+        mGenreRepository = DaggerDatabaseComponent.builder()
+                .applicationModule
+                        (new ApplicationModule(applicationContext)).build().getGenreRepository();
+    }
+
+    @Override
+    public void onCreate() {}
+
+    @Override
+    public void onDataSetChanged() {
+        Log.d(TAG, "Getting data");
+        final long token = Binder.clearCallingIdentity();
+        try {
+            movies = mMovieRepository.getMovies();
+        } finally {
+            Binder.restoreCallingIdentity(token);
+        }
+        Log.d(TAG, "Movies size: "+ movies.size());
+    }
+
+    @Override
+    public void onDestroy() {
+    }
+
+    @Override
+    public int getCount() {
+        Log.d(TAG, "Count called");
+        if(movies == null) {
+            return 0;
+        }
+
+        Log.d(TAG, "mRecipes size: "+ movies.size());
+        return movies.size();
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+        Log.d(TAG, "getViewAt: "+ position);
+        RemoteViews views = new RemoteViews(mContext.getPackageName(), R.layout.item_recipe_widget);
+
+        Movie movie = movies.get(position);
+        views.setTextViewText(R.id.text_widget_movie_name, movie.getName());
+
+        final long token = Binder.clearCallingIdentity();
+        try {
+            List<Genre> genres = mGenreRepository.getGenresByMovieId(movie.getMovieId());
+            List<String> genresName = new ArrayList<>();
+            for (Genre genre : genres) {
+                genresName.add(genre.getName());
+            }
+            StringBuilder genreDuration = new StringBuilder(TextUtils.join(", ", genresName));
+            genreDuration.append(" | ");
+            genreDuration.append(String.valueOf(movie.getDuration()));
+            genreDuration.append(" min");
+            views.setTextViewText(R.id.text_movie_genre_duration, genreDuration);
+
+            if (movie.getRating() != null &&
+                    movie.getRating().getImdb() != null &&
+                    movie.getRating()
+                            .getImdb().length() > 0) {
+                views.setTextViewText(R.id.text_imdb_value, movie.getRating().getImdb());
+            }
+            if (movie.getRating() != null &&
+                    movie.getRating().getRottenTomatoes() != null &&
+                    movie.getRating().getRottenTomatoes().length() > 0) {
+                views.setTextViewText(R.id.text_rotten_tomatoes_value, movie.getRating().getRottenTomatoes());
+
+
+            }
+            return views;
+        } finally {
+            Binder.restoreCallingIdentity(token);
+        }
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+}
