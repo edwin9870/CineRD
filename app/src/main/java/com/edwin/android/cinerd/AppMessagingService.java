333e6b352ad1e635e7e695474939b1175203713e
Edwin Ramirez Ventura <edwin9870@gmail.com>
2017-08-13 10:27:28 -0400

    Add settings option to menu poster

---
+++ app/src/main/java/com/edwin/android/cinerd/AppMessagingService.java
@@ -0,0 +1,20 @@
+package com.edwin.android.cinerd;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by Edwin Ramirez Ventura on 8/12/2017.
+ */
+
+public class AppMessagingService extends FirebaseMessagingService {
+
+    public static final String TAG = AppMessagingService.class.getSimpleName();
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.d(TAG, "Message received...");
+    }
+}
